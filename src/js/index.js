782821bdd7a6a067b204fbc328a0e49c528fb28d
ghost <ghost@pop-os.localdomain>
2023-01-28 09:27:03 -0500

    Working on homepage

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 console.log("Hello World!")
+
+// HAMBURGER MENU ->
+const hamburger = document.getElementById('hamburger_icons')
+
+hamburger.addEventListener("click", event => {
+    event.preventDefaults
+    alert('you clicked the hamburger!')
+    // SLIDE OUT MENU ->
+})
+
+hamburger.addEventListener("mouseover", event => {
+    event.preventDefault
+    hamburger.style.color = "white"
+})
+
+// IMAGE SLIDESHOW ->
