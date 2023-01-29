ed64ee3f68a45cc51428afb4bf7385e9d6e8fd77
ghost <ghost@pop-os.localdomain>
2023-01-29 14:25:14 -0500

    Working on homepage image slider, added jquery and slick

--- src/js/index.js
+++ src/js/index.js
@@ -15,3 +15,24 @@ hamburger.addEventListener("mouseover", event => {
 })
 
 // IMAGE SLIDESHOW ->
+$(document).ready(() => {
+    $('#image_slider-container').slick({
+        autoplay: true,
+        lazyLoad: true,
+    })
+})
+
+// let slider_images = document.querySelectorAll(".image_slider-images")
+// let imageArr = Array.from(slider_images)
+// console.log(imageArr)
+// function nextImage() {
+//     for(let index=0; index < imageArr.length; index++) {
+//         console.log(index)
+//         imageArr[index].style = 'display: block;'
+//         setTimeout(() => {
+//             imageArr[index.style = 'display: none;']
+//             requestAnimationFrame(nextImage)
+//         }, 3000)
+//     }
+// }
+// window.requestAnimationFrame(nextImage)
